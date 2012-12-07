b55603502e56fa74fc946618fdc6a29b8075b0d0
Kelvin Chappell <kelvin.chappell@guardian.co.uk>
2012-12-07 15:58:00 +0000

    Added some reminders.

--- src/main/scala/specs2/DataTableBuilderClient.scala
+++ src/main/scala/specs2/DataTableBuilderClient.scala
@@ -1,18 +1,23 @@
 package specs2
 
+// TODO: quotes around strings
+// TODO: pad each column out to its max value length
 object DataTableBuilderClient extends App {
 
   val cantHappen = new Exclusion(((combination: List[Any]) => combination == List("a", 1, true, "randomrandomrandom2")
-    || combination(1).asInstanceOf[Int] > 3),
+    || combination(1).asInstanceOf[Int] > 2),
     "This can't happen")
 
+  val illegal = new Exclusion(((combination: List[Any]) => combination == List("a", 1, true, "randomrandomrandom2")
+    || combination(1).asInstanceOf[Int] == 4),
+    "This is illegal")
+
   new DataTableBuilder(
-    List(
+    values = List(
       Set("a", "b", "c"),
       Set(1, 2, 3, 4),
       Set(true, false),
-      Set("randomrandomrandom1", "randomrandomrandom2", "randomrandomrandom3", "randomrandomrandom4", "randomrandomrandom5")),
-    List(cantHappen))
-    .build
-    .foreach(println)
+      Set("rand", "random", "randomrandomrandom2", "randomrandomrandom4", "randomrandomrandom5")),
+    exclusions = illegal :: cantHappen :: Nil
+  ).build.foreach(println)
 }
